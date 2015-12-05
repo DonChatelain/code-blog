e84f85b5c0c1d19c7394dc8246d52bf8c9e5a270
DonChatelain <djchatelain@gmail.com>
2015-12-04 16:55:12 -0800

    styles and refines new post page, but Raw HTML is broken, probaby b/c of highlighter

--- Js/newPost.js
+++ Js/newPost.js
@@ -1,59 +1,40 @@
 
 var NEW_BLOG_POST_MODULE = (function() {
+
     var my = {};
-    /* 
-       var marked = require('marked');
-    marked.setOptions({
-	renderer: new marked.Renderer(),
-	gfm: true,
-	tables: true,
-	breaks: false,
-	pedantic: false,
-	sanitize: true,
-	smartLists: true,
-	smartypants: false,
-	forceid: false
-    });
-    */
-    
+
     var $postBodyAttribute = $('#postBodyAttribute');
     var $intermediateHTML = $('#intermediateHTML');
     var $output = $('#postOutput');
     var $jsonOutput = $('#jsonOutput');
     var markdownObject = {};
 
+//converts date string format to conform to existing blog.rawData
     my.pad = function(num, size) {
     var s = num+"";
     while (s.length < size) s = "0" + s;
     return s;
-    }
+    };
 
     my.render = function() {
-	console.log("Here I am");
-	var postBodyAttribute = $postBodyAttribute.val();
-	my.markdown = marked(postBodyAttribute);
-	console.log(my.markdown);
-	$intermediateHTML.text(my.markdown);
-	$output.html(my.markdown);
-	markdownObject.body = my.markdown;
-	console.log(markdownObject);
-	$jsonOutput.text(JSON.stringify(markdownObject));
-	
-    }
-    
+
+    	var postBodyAttribute = $postBodyAttribute.val();
+    	my.markdown = marked(postBodyAttribute);
+    	$intermediateHTML.text(my.markdown);
+    	$output.html(my.markdown);
+    	markdownObject.body = my.markdown;
+    	$jsonOutput.text(JSON.stringify(markdownObject));
+    };
+
     my.getInput = function() {
-	markdownObject.title = $('#titleInput').val();
-	markdownObject.author = $('#authorInput').val();
-	markdownObject.category = $('#categoryInput').val();
-	var date = new Date();
-	markdownObject.publishedOn = date.getFullYear() + "-"
-	    + my.pad( date.getMonth() + 1, 2 ) + "-" + my.pad(date.getDate(), 2);
-	console.log(markdownObject.publishedOn);
-	my.render();
-    }
+    	markdownObject.title = $('#titleInput').val();
+    	markdownObject.author = $('#authorInput').val();
+    	markdownObject.category = $('#categoryInput').val();
+    	var date = new Date();
+    	markdownObject.publishedOn = date.getFullYear() + "-" + my.pad( date.getMonth() + 1, 2 ) + "-" + my.pad(date.getDate(), 2);
+    	my.render();
+    };
 
-
-	
     $postBodyAttribute.on( 'input', my.render );
 
     my.render();

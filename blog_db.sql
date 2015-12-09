327781d6ff5c32e56a57b62582b46a2a1ef7faad
DonChatelain <djchatelain@gmail.com>
2015-12-09 15:16:50 -0800

    FUCK

---
+++ blog_db.sql
@@ -0,0 +1,3 @@
+CREATE TABLE blog_table (id INTEGER PRIMARY KEY, title varchar(255), category varchar(255), author varchar(255), authorUrl varchar(255), publishedOn varchar(255), body varchar(255));
+
+INSERT INTO blog_table (1, 'exampleTitle', 'exampleCategory', 'exampleAuthor', 'exampleUrl', 'exampleDate', 'exampleBody');
